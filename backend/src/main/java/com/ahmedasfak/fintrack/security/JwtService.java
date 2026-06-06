3e7e6fd6064b621e480a0d27e1d44a1a86ea441b
ahmedasfak2001 <quaderiasfak@gmail.com>
2026-06-06 11:51:16 +0530

    Added JWT token validation methods

--- backend/src/main/java/com/ahmedasfak/fintrack/security/JwtService.java
+++ backend/src/main/java/com/ahmedasfak/fintrack/security/JwtService.java
@@ -2,21 +2,21 @@ package com.ahmedasfak.fintrack.security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "mySuperSecretKeyForFinTrackApplication2026JwtToken";
+    private static final String SECRET_KEY = "mySuperSecretKeyForFinTrackApplication2026JwtToken";
 
     private final Key key = Keys.hmacShaKeyFor(
-            SECRET_KEY.getBytes()
-    );
+            SECRET_KEY.getBytes());
 
     public String generateToken(String email) {
 
@@ -25,9 +25,48 @@ public class JwtService {
                 .issuedAt(new Date())
                 .expiration(
                         new Date(System.currentTimeMillis()
-                                + 1000 * 60 * 60 * 24)
-                )
+                                + 1000 * 60 * 60 * 24))
                 .signWith(key)
                 .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver) {
+
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith((javax.crypto.SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(
+            String token,
+            String email) {
+
+        final String extractedEmail = extractEmail(token);
+
+        return extractedEmail.equals(email)
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(
+            String token) {
+
+        return extractClaim(
+                token,
+                Claims::getExpiration)
+                .before(new Date());
     }
 }
