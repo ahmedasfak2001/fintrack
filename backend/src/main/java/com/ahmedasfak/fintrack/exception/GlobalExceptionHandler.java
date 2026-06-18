472b7dc8d2ac962730caec37faeb1f2b21779314
ahmedasfak2001 <quaderiasfak@gmail.com>
2026-06-18 19:52:40 +0530

    Add global exception handling

---
+++ backend/src/main/java/com/ahmedasfak/fintrack/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.ahmedasfak.fintrack.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ahmedasfak.fintrack.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse>
+    handleRuntimeException(
+            RuntimeException ex) {
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse>
+    handleException(
+            Exception ex) {
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "Something went wrong");
+
+        return ResponseEntity
+                .status(
+                        HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+}
