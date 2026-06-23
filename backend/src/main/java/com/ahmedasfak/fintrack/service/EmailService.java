5889a6699410b56069673b5ddc6d63d972f7e44c
ahmedasfak2001 <quaderiasfak@gmail.com>
2026-06-23 21:23:09 +0530

    Trying to fix email verification till now :(

--- backend/src/main/java/com/ahmedasfak/fintrack/service/EmailService.java
+++ backend/src/main/java/com/ahmedasfak/fintrack/service/EmailService.java
@@ -66,7 +66,7 @@ public class EmailService {
 
                         e.printStackTrace();
 
-                        throw e;
+                        // throw e;
                 }
         }
         // public void sendPasswordResetEmail(
